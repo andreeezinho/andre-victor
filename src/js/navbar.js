6a67546d96f65edbafb010307835bf0ca027a399
andreeezinho <andreeesapucaia@gmail.com>
2024-08-10 11:52:03 -0300

    section responsiva ok(?)

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -2,10 +2,17 @@ var fechar = document.getElementById("fechar").addEventListener("click", fecharM
 var menuMobile = document.getElementById("menuMobile");
 var barras = document.getElementById("barras").addEventListener("click", abrirMenu);
 
+//resolver bug da linha aparecendo
+var linha = document.getElementById("linha");
+
 function abrirMenu(){
+    //abrir a barra lateral mobile
     menuMobile.style.display = "flex";
+    linha.style.display = "none";
 }
 
 function fecharMenu(){
+    //fechar a barra lateral mobile
     menuMobile.style.display = "none";
+    linha.style.display = "inline";
 }
